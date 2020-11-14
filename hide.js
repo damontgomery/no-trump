a688bcc6e4c7119f9345d3ea8b7c3d258c601f9b
Daniel Montgomery <damontgomery@gmail.com>
2020-11-14 01:21:09 -0600

    replace blacklisted words.

--- hide.js
+++ hide.js
@@ -2,7 +2,40 @@
 
 if (document.title.toLowerCase().includes('trump')) {
   // Hero image.
-  document.querySelectorAll('.e-image--hero img').forEach(function(el, i){
+  document.querySelectorAll('.e-image--hero img').forEach((el, i) => {
     el.classList.add('hide-trump');
   });
 }
+
+// General text with `Trump`.
+const blacklistedWords = [
+  'Trump',
+];
+
+function getTextNodes() {
+  let walker = document.createTreeWalker(
+      document.body, 
+      NodeFilter.SHOW_TEXT, 
+      null, 
+      false
+  );
+
+  let node;
+  let textNodes = [];
+
+  while(node = walker.nextNode()) {
+      textNodes.push(node);
+  }
+
+  return textNodes;
+}
+
+let textNodes = getTextNodes();
+
+textNodes.forEach((el) => {
+  blacklistedWords.forEach((word) => {
+    const replacement = '*'.repeat(word.length);
+
+    el.nodeValue = el.nodeValue.replaceAll(word, replacement);
+  });
+});
